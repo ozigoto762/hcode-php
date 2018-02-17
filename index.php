b064423393506dba9e8b5aba26f868dd35fb20c2
Marcelo <ozigoto762@gmail.com>
2018-02-17 17:34:37 -0200

    Criei o index

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP - Hcode</title>
+	<style type="text/css">
+		h1{
+			font-size: 26px;
+			color:#00008B;
+			font-family: arial;
+		}
+		#titulo{
+			margin: 15px;
+			padding: 10px;
+			background-color: #fff8dc;
+			text-align: center;
+		}
+	</style>
+</head>
+<body>
+	<div id='titulo'>
+		<?php
+			echo "<h1>Hello World!! </h1>";
+		?>
+	</div>
+
+</body>
+</html>
+
